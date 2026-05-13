bf07daf961c59ca96c31dff367ba68b637294351
Vladimir <vovandrop@gmail.com>
2026-05-13 16:11:24 +0300

    Chore: clean repository from generated caches and temp files

--- .codex_cost_probe.gd
+++
@@ -1,6 +0,0 @@
-﻿extends SceneTree
-func _init():
-	for id in GameState.get_equipment_ids():
-		var unlocked := GameState.is_equipment_unlocked(id)
-		print(str(id), ' unlocked=', unlocked, ' unlock_cost=', GameState.get_equipment_unlock_cost(id), ' upgrade_cost=', GameState.get_equipment_upgrade_cost(id))
-	quit()
